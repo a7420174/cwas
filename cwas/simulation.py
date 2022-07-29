a7647e143303330eb4b74cbbcee0c26f7ccdd91a
a7420174 <a7420174@gmail.com>
2022-07-29 14:08:11 +0900

    Check adj_factor args

--- cwas/simulation.py
+++ cwas/simulation.py
@@ -99,6 +99,8 @@ class Simulation(Runnable):
     def _check_args_validity(args: argparse.Namespace):
         check_is_file(args.sample_info_path)
         check_num_proc(args.num_proc)
+        if args.adj_factor_path is not None:
+            check_is_file(args.adj_factor_path)
 
     @property
     def in_vcf_path(self) -> Path:
